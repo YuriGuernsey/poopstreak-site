4fd177b0f7c0757afde6cc1441df8a1626aae174
Yuri Alves <yurialvesguernsey@outlook.com>
2022-10-21 17:45:35 +0100

    Update app.js

--- app.js
+++ app.js
@@ -7,29 +7,27 @@ const database = supabase.createClient(url, key);
 let save = document.querySelector("#save");
 save.addEventListener("click", async (e) => {
     e.preventDefault();
-    let name = document.querySelector("#name").value;
-    let age = document.querySelector("#age").value;
-    let country = document.querySelector("#country").value;
+    let title = document.querySelector("#title").value;
+    let post = document.querySelector("#post").value;
     save.innerText = "Saveing....";
     save.setAttribute("disabled", true);
-    let res = await database.from("students").insert({
-        name: name,
-        age: age,
-        country: country
+    let res = await database.from("posts").insert({
+        title: title,
+        post: post
     })
     if (res) {
         alert("Student Add Successfully")
         save.innerText = "Save"
         save.setAttribute("disabled", false);
-        name = "";
-        age = "";
-        country = "";
-        getStudent();
-        getTotalCount();
+        title = "";
+        post = "";
+       
+        getPosts();
+       
 
 
     } else {
-        alert("Student Not Add Successfully")
+        alert("Post Not Add Successfully")
         save.innerText = "Save"
         save.setAttribute("disabled", false);
     }
@@ -50,55 +48,55 @@ const getPosts = async () => {
 
 getPosts();
 
-const getTotalCount = async () => {
-    let total = document.querySelector("#total");
-    const res = await database.from("students").select("*", { count: "exact" });
-    total.innerText = res.data.length;
-}
+// const getTotalCount = async () => {
+//     let total = document.querySelector("#total");
+//     const res = await database.from("students").select("*", { count: "exact" });
+//     total.innerText = res.data.length;
+// }
 
-getTotalCount();
+// getTotalCount();
 
-const editStudent = async (id) => {
+// const editStudent = async (id) => {
 
 
-    const res = await database.from("students").select("*").eq("id", id);
-    if (res) {
-        document.getElementById("id").value = res.data[0].id;
-        document.getElementById("edit-name").value = res.data[0].name;
-        document.getElementById("edit-age").value = res.data[0].age;
-        document.getElementById("edit-country").value = res.data[0].country;
-    }
-}
+//     const res = await database.from("students").select("*").eq("id", id);
+//     if (res) {
+//         document.getElementById("id").value = res.data[0].id;
+//         document.getElementById("edit-name").value = res.data[0].name;
+//         document.getElementById("edit-age").value = res.data[0].age;
+//         document.getElementById("edit-country").value = res.data[0].country;
+//     }
+// }
 
-const update = document.getElementById("update");
+// const update = document.getElementById("update");
 
-update.addEventListener("click", async () => {
-    let id = document.getElementById("id").value;
-    let name = document.getElementById("edit-name").value
-    let age = document.getElementById("edit-age").value;
-    let country = document.getElementById("edit-country").value;
-    update.innerText = "Updateing...."
-    update.setAttribute("disabled", true);
-    const res = await database.from("students").update({
-        name, age, country
-    }).eq("id", id)
+// update.addEventListener("click", async () => {
+//     let id = document.getElementById("id").value;
+//     let name = document.getElementById("edit-name").value
+//     let age = document.getElementById("edit-age").value;
+//     let country = document.getElementById("edit-country").value;
+//     update.innerText = "Updateing...."
+//     update.setAttribute("disabled", true);
+//     const res = await database.from("students").update({
+//         name, age, country
+//     }).eq("id", id)
 
-    if (res) {
-        alert("Student Update Successfully")
-        update.innerText = "Update"
-        update.setAttribute("disabled", false);
-        name = "";
-        age = "";
-        country = "";
-        getStudent();
-        getTotalCount();
+//     if (res) {
+//         alert("Student Update Successfully")
+//         update.innerText = "Update"
+//         update.setAttribute("disabled", false);
+//         name = "";
+//         age = "";
+//         country = "";
+//         getStudent();
+//         getTotalCount();
 
-    } else {
-        alert("Student Not Update Successfully")
-        update.innerText = "Update"
-        update.setAttribute("disabled", false);
-    }
-})
+//     } else {
+//         alert("Student Not Update Successfully")
+//         update.innerText = "Update"
+//         update.setAttribute("disabled", false);
+//     }
+// })
 
 
 const deleteStudent = async (id) => {
